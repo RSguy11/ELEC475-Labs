012652bcc21f104d8f59f3ce88d43f58f68fe98b
RSguy11 <naeslawrence@gmail.com>
2025-11-09 20:47:53 -0500

    Adding Dataset to gitignore since its to big for pascal voc 2012

---
+++ Lab3/voc_download.py
@@ -0,0 +1,7 @@
+#pip install kagglehub
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("gopalbhattrai/pascal-voc-2012-dataset")
+
+print("Path to dataset files:", path)
